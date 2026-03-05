3c01495b4cbe46066f6c7f9a037349e86301c4d9
suman patari <patarisuman05@gmail.com>
2026-03-05 10:16:32 +0530

    Implement UC1: Application Entry & Welcome Message

---
+++ src/PalindromeCheckerApp.java
@@ -0,0 +1,11 @@
+public class PalindromeCheckerApp {
+    public static void main(String[] args) {
+        // UC1: Application Entry & Welcome Message
+        System.out.println("--- Welcome to Palindrome Checker App ---");
+        System.out.println("Application Name: Palindrome Checker");
+        System.out.println("Application Version: 1.0");
+        System.out.println("-----------------------------------------");
+
+        // Next use cases will be added here
+    }
+}
